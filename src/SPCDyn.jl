ea1cac68456404f7273a3de0371b1c050ee68dcc
Sean P. Cornelius <spcornelius@gmail.com>
2023-08-02 10:35:22 -0500

    add fixed point utilities

--- src/SPCDyn.jl
+++ src/SPCDyn.jl
@@ -4,11 +4,13 @@ using Distances
 using FastBroadcast
 using Graphs
 using LinearAlgebra
+using NLsolve
 using SparseArrays
 using StaticArrays
 using UnPack
 
 include("./base.jl")
+include("./fixed_points.jl")
 include("./kuramoto.jl")
 include("./magnetic_pendulum.jl")
 
---
+++ src/fixed_points.jl
@@ -0,0 +1,26 @@
+export find_fixed_point, is_stable
+
+function find_fixed_point(sys::sysType, x₀::AbstractVector{T}; kw...) where 
+    {sysType <: AutonomousODESys, T}
+    f! = (F, x) -> rhs(F, x, sys)
+    result = if hasmethod(jac, (AbstractMatrix, sysType, AbstractVector{T}))
+        n = length(x₀)
+        J = zeros(T, n, n)
+        j! = (J, x) -> jac(J, x, sys)
+        nlsolve(f!, j!, x₀; kw...)
+    else
+        nlsolve(f!, x₀; kw...)
+    end
+    result.f_converged || 
+        error("Numerical optimization failed.")
+    return result.zero
+end
+
+function is_stable(sys::AutonomousODESys, x::AbstractVector{T}; 
+                   tol = zero(T)) where {T}
+    n = length(x)
+    J = zeros(T, n, ns)
+    jac(J, x, sys)
+    λs = eigvals!(J)
+    return maximum(real, λs) < -tol
+end
